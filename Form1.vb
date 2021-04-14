19342a30d2bd38da5faf63d3f2a4589ff9c94807
MuhdDanish <82487726+MuhdDanish@users.noreply.github.com>
2021-04-14 11:48:01 +0800

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,27 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim strNamaHari(6) As String
+        Dim sum As Integer
+
+        strNamaHari(0) = "Ahad"
+        strNamaHari(1) = "Isnin"
+        strNamaHari(2) = "Selasa"
+        strNamaHari(3) = "Rabu"
+        strNamaHari(4) = "Khamis"
+        strNamaHari(5) = "Jumaat"
+        strNamaHari(6) = "Sabtu"
+
+        System.Array.Reverse(strNamaHari)
+
+        For sum = 0 To 6
+            ListHari.Items.Add(strNamaHari(sum))
+        Next
+
+
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListHari.SelectedIndexChanged
+
+
+    End Sub
+End Class
